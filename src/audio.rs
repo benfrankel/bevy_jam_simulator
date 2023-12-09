38ee326d98aebc33bc20ccdd0e47a5ec47dc82fa
necrashter <iiilker99@gmail.com>
2023-12-09 14:01:42 +0300

    Load upgrade sfx into a Vec

--- src/audio.rs
+++ src/audio.rs
@@ -2,8 +2,8 @@ use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
 use bevy_kira_audio::prelude::*;
 use bevy_kira_audio::AudioPlugin as KiraAudioPlugin;
+use rand::seq::SliceRandom;
 use rand::thread_rng;
-use rand::Rng;
 
 pub struct AudioPlugin;
 
@@ -20,23 +20,18 @@ impl Plugin for AudioPlugin {
 #[derive(AssetCollection, Resource, Reflect, Default)]
 #[reflect(Resource)]
 pub struct AudioAssets {
-    #[asset(path = "audio/upgrade0.ogg")]
-    pub upgrade0: Handle<AudioSource>,
-    #[asset(path = "audio/upgrade1.ogg")]
-    pub upgrade1: Handle<AudioSource>,
+    #[asset(paths("audio/upgrade0.ogg", "audio/upgrade1.ogg"), collection(typed))]
+    pub upgrade_sounds: Vec<Handle<AudioSource>>,
     #[asset(path = "music/ingame.ogg")]
     pub music: Handle<AudioSource>,
 }
 
 impl AudioAssets {
     pub fn random_upgrade(&self) -> Handle<AudioSource> {
-        let rng = &mut thread_rng();
-        let rand: f32 = rng.gen_range(0.0..1.0);
-        if rand < 0.5 {
-            self.upgrade0.clone()
-        } else {
-            self.upgrade1.clone()
-        }
+        self.upgrade_sounds
+            .choose(&mut thread_rng())
+            .unwrap()
+            .clone()
     }
 }
 
